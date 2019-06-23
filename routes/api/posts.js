19a865d7959dad20bb7d73339a768599c1caff5f
Jon Christensen <jonchristensen11@gmail.com>
2019-06-22 22:31:15 -0500

    Added Comment API Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -28,6 +28,7 @@ router.post('/', [auth, [
 
     const newPost = {
       text: req.body.text,
+      name: user.name,
       avatar: user.avatar,
       user: req.user.id
     }
@@ -147,51 +148,71 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 })
 
-// @route  PUT api/posts/dislike/:id
-// @desc   Dislike a post
+
+
+// @route  POST api/posts/comment/:id
+// @desc   Comment on a post
 // @access Private
-router.put('/dislike/:id', auth, async (req, res) => {
+router.post('/comment/:id', [auth, [
+  check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() })
+  }
+
   try {
+    const user = await User.findById(req.user.id).select('-password');
     const post = await Post.findById(req.params.id);
 
-    if (post.dislikes.filter(dislike => dislike.user.toString() === req.user.id).length > 0) {
-      return res.json(400).json({ msg: 'Post already disliked' });
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
     }
 
-    post.dislikes.unshift({ user: req.user.id })
+    post.comments.unshift(newComment);
 
     await post.save();
 
-    res.json(post.dislikes);
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
-})
+});
 
-// @route  PUT api/posts/undislike/:id
-// @desc   Undislike a post
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   Delete comment
 // @access Private
-router.put('/undislike/:id', auth, async (req, res) => {
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
 
-    if (post.dislikes.filter(dislike => dislike.user.toString() === req.user.id).length === 0) {
-      return res.json(400).json({ msg: 'Post has not yet been disliked' });
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment does not exist' })
     }
 
-    const removeIndex = post.dislikes.map(dislike => dislike.user.toString()).indexOf(req.user.id);
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
 
-    post.dislikes.splice(removeIndex, 1)
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1)
 
     await post.save();
 
-    res.json(post.dislikes);
+    res.json(post.comments);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
-})
+});
 
 
 module.exports = router;
