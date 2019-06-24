9af8f63db6e2d820b0a8559aa1536a63aba5d2ca
Jon Christensen <jonchristensen11@gmail.com>
2019-06-23 21:18:37 -0500

    Added React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () =>
-  <Fragment>
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Route exact path="/" component={Landing} />
+      <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Switch>
+    </Fragment>
+  </Router>
 
 export default App;
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -6,8 +7,8 @@ const Landing = () => {
       <div className="landing-inner">
         <h1 className="landing-brand">Chatter</h1>
         <div className="landing-buttons">
-          <a href="register.html" className="btn btn-landing">Sign Up</a>
-          <a href="login.html" className="btn btn-landing">Login</a>
+          <Link to="/register" className="btn btn-landing">Sign Up </Link>
+          <Link to="/login" className="btn btn-landing">Login </Link>
         </div>
       </div>
     </section>
