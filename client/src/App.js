40e006abee3c7af92ab116177a23897bfab7bf88
Jon Christensen <jonchristensen11@gmail.com>
2019-06-24 16:07:36 -0500

    Initial Redux Setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,20 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 const App = () =>
-  <Router>
-    <Fragment>
-      <Route exact path="/" component={Landing} />
-      <Switch>
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </Fragment>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Route exact path="/" component={Landing} />
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </Fragment>
+    </Router>
+  </Provider>
 
 export default App;
