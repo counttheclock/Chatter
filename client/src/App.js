d78c9ca170ee5139a78c3a8c2c7a640a0aabd862
Jon Christensen <jonchristensen11@gmail.com>
2019-06-24 20:55:45 -0500

    Added Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-import './App.css';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from './actions/auth';
 
-const App = () =>
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Route exact path="/" component={Landing} />
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Fragment>
-    </Router>
-  </Provider>
+import setAuthToken from './utils/setAuthToken';
+import './App.css';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Route exact path="/" component={Landing} />
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </Fragment>
+      </Router>
+    </Provider>
+  )
+}
+
 
 export default App;
