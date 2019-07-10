309d2acac6c035668f904a2fcd8e224f4901a641
counttheclock <46055761+counttheclock@users.noreply.github.com>
2019-07-10 13:56:57 -0500

    Made Sure Navbar was Hidden From Landing Page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,7 +9,30 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout, location }) => {
   if (location.pathname.match('/login') || location.pathname.match('/register')) {
     return null;
   }
-  return (
+  return location.pathname === '/' ?
+    <div className="hide">
+      <nav>
+        <div className="logo">
+          <Link to="/dashboard"><img className="logo-img" src={logo} alt="logo" /></Link>
+        </div>
+        <div>
+        </div>
+        <div className="account-btn-wrapper">
+          <Link to="/profiles" className="settings-btn">Profiles</Link>
+          <Link to="/settings" className="settings-btn">Account</Link>
+          <button onClick={logout} className="logout-btn">Logout</button>
+          <Link to="/notifications" className="nav-note-btn"><i className="fas fa-bell"></i></Link>
+          <input className="menu-btn" type="checkbox" id="menu-btn" />
+          <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+          <ul className="menu">
+            <li><Link className="menu-link" to="/profiles">Profiles</Link></li>
+            <li><Link className="menu-link" to="/settings">Account</Link></li>
+            <li><button className="menu-link logout-smbtn" onClick={logout}>Logout</button></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+    :
     <div>
       <nav>
         <div className="logo">
@@ -32,7 +55,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout, location }) => {
         </div>
       </nav>
     </div>
-  )
 }
 
 Navbar.propTypes = {
